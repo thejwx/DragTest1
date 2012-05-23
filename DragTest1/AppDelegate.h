5b3ce446670b3f37e0a2701558ddc044b2a46ce7
Jeff Wilson <jeff.wilson@tx-002226-ap.frogdesign.com>
2012-05-23 12:59:15 -0500

    Initial Commit

---
+++ DragTest1/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  DragTest1
+//
+//  Created by frogTemplate on 5/23/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ DragTest1/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DragTest1
+//
+//  Created by frogTemplate on 5/23/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
